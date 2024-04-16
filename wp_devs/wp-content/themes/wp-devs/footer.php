1a1074a63f02707065eaae5d1103571548f4820b
Timothy Karani <kartimothy@gmail.com>
2024-04-16 13:20:32 +0300

    added functions.php to enqueue style sheets

---
+++ wp_devs/wp-content/themes/wp-devs/footer.php
@@ -0,0 +1,5 @@
+<footer class="site-footer">Footer</footer>
+</div>
+</body>
+
+</html>
---
+++ wp_devs/wp-content/themes/wp-devs/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head() ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique praesentium, nostrum obcaecati nesciunt adipisci repudiandae aliquid quidem optio numquam quaerat modi laborum exercitationem? Earum ipsum, maiores alias ex nobis et.</p>
+
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="searchbox">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
