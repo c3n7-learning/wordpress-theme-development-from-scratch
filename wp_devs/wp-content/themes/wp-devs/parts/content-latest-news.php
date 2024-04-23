3e230a8885708dc2745255d86ed3246b3bc6f6fd
Timothy Karani <kartimothy@gmail.com>
2024-04-24 02:46:50 +0300

    added conditionals to conentent partial

--- wp_devs/wp-content/themes/wp-devs/parts/content-latest-news.php
+++ wp_devs/wp-content/themes/wp-devs/parts/content-latest-news.php
@@ -1,15 +1,22 @@
 <article class="latest-news">
-    <a href="<?php the_permalink(); ?>">
-        <?php the_post_thumbnail('large'); ?>
-    </a>
+    <!--  https://codex.wordpress.org/Conditional_Tags -->
+    <?php if (has_post_thumbnail()) : ?>
+        <a href="<?php the_permalink(); ?>">
+            <?php the_post_thumbnail('large'); ?>
+        </a>
+    <?php endif; ?>
     <a href="<?php the_permalink(); ?>">
         <h3><?php the_title(); ?></h3>
     </a>
     <div class="meta-info">
         <p>
             by <span><?php the_author_posts_link(); ?></span>
-            Categories <span> <?php the_category(' ') ?> </span>
-            Tags: <?php the_tags('', ', ') ?>
+            <?php if (has_category()) : ?>
+                Categories <span> <?php the_category(' ') ?> </span>
+            <?php endif; ?>
+            <?php if (has_tag()) : ?>
+                Tags: <?php the_tags('', ', ') ?>
+            <?php endif; ?>
         </p>
         <p><span><?php echo get_the_date(); ?></span></p>
     </div>
