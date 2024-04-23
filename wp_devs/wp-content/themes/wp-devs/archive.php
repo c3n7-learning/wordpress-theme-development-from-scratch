3506c211cf7ff46bcc276d20c7c47d76974d5ea5
Timothy Karani <kartimothy@gmail.com>
2024-04-23 15:47:16 +0300

    refactored code into template parts

--- wp_devs/wp-content/themes/wp-devs/archive.php
+++ wp_devs/wp-content/themes/wp-devs/archive.php
@@ -17,26 +17,9 @@
                     if (have_posts()) {
                         while (have_posts()) {
                             the_post();
-                    ?>
-                            <article>
-                                <h2>
-                                    <a href="<?php the_permalink(); ?>">
-                                        <?php the_title(); ?>
-                                    </a>
-                                </h2>
-                                <a href="<?php the_permalink(); ?>">
-                                    <?php the_post_thumbnail([275, 275]); ?>
-                                </a>
-                                <div class="meta-info">
-                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?> </p>
-                                    <p>Categories <?php the_category(' ') ?></p>
-                                    <p>Tags: <?php the_tags('', ', ') ?></p>
-                                </div>
-                                <?php the_excerpt(); ?>
-                            </article>
-                        <?php
+                            get_template_part('parts/content');
                         }
-                        ?>
+                    ?>
                         <div class="wpdevs-pagination">
                             <div class="pages new">
                                 <?php previous_posts_link("<< Newer posts") ?>
--- wp_devs/wp-content/themes/wp-devs/page-home.php
+++ wp_devs/wp-content/themes/wp-devs/page-home.php
@@ -48,25 +48,7 @@
                     if ($postList->have_posts()) {
                         while ($postList->have_posts()) {
                             $postList->the_post();
-                    ?>
-                            <article class="latest-news">
-                                <a href="<?php the_permalink(); ?>">
-                                    <?php the_post_thumbnail('large'); ?>
-                                </a>
-                                <a href="<?php the_permalink(); ?>">
-                                    <h3><?php the_title(); ?></h3>
-                                </a>
-                                <div class="meta-info">
-                                    <p>
-                                        by <span><?php the_author_posts_link(); ?></span>
-                                        Categories <span> <?php the_category(' ') ?> </span>
-                                        Tags: <?php the_tags('', ', ') ?>
-                                    </p>
-                                    <p><span><?php echo get_the_date(); ?></span></p>
-                                </div>
-                                <?php the_excerpt(); ?>
-                            </article>
-                    <?php
+                            get_template_part('parts/content', 'latest-news');
                         }
                         wp_reset_postdata();
                     } else {
