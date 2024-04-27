2ef0b6c3fac579f6c0ca0d28e1fd6d74f0597153
Timothy Karani <kartimothy@gmail.com>
2024-04-27 03:11:25 +0300

    dabbled with child themes

---
+++ wp_devs/wp-content/themes/wp-devs-child/footer.php
@@ -0,0 +1,12 @@
+<footer class="site-footer">
+    <div class="container">
+        <nav class="footer-menu">
+            <?php wp_nav_menu(['theme_location' => 'wp_devs_footer_menu', 'depth' => 1]) ?>
+        </nav>
+    </div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+
+</html>
