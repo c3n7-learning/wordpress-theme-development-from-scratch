f920af06a88a11fc26ffc41ba189f6cee28a83f6
Timothy Karani <kartimothy@gmail.com>
2024-04-27 18:43:04 +0300

    done with wp_devs course

--- wp_devs/wp-content/themes/wp-devs/functions.php
+++ wp_devs/wp-content/themes/wp-devs/functions.php
@@ -49,25 +49,25 @@ function wpdevs_config()
 
     add_theme_support('title-tag');
 
-    add_theme_support('align-wide');
+    // add_theme_support('align-wide');
     add_theme_support('responsive-embeds');
     add_theme_support('editor-styles');
     add_editor_style('style-editor.css');
 
-    add_theme_support('wp-block-styles');
-    add_theme_support('editor-color-palette', [
-        [
-            'name'  => __('Primary', 'wp-devs'),
-            'slug'  => 'primary',
-            'color' => '#001E32'
-        ],
-        [
-            'name'  => __('Secondary', 'wp-devs'),
-            'slug'  => 'secondary',
-            'color' => '#CFAF07'
-        ]
-    ]);
-    add_theme_support('disable-custom-colors');
+    // add_theme_support('wp-block-styles');
+    // add_theme_support('editor-color-palette', [
+    //     [
+    //         'name'  => __('Primary', 'wp-devs'),
+    //         'slug'  => 'primary',
+    //         'color' => '#001E32'
+    //     ],
+    //     [
+    //         'name'  => __('Secondary', 'wp-devs'),
+    //         'slug'  => 'secondary',
+    //         'color' => '#CFAF07'
+    //     ]
+    // ]);
+    // add_theme_support('disable-custom-colors');
 }
 
 add_action('after_setup_theme', 'wpdevs_config', 0);
